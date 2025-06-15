f58879cb4352983f061592b643847a6c0167b5c1
AyalaDruk <ayalahorberg1999@gmail.com>
2025-06-15 21:24:58 +0300

    add hw and less09  parametrize and fixture

--- HW_09/tests/test_calculate_discount.py
+++ HW_09/tests/test_calculate_discount.py
@@ -2,13 +2,11 @@ import pytest
 from HW_09.pages.calculate_discount import calculate_discount
 
 test_data = [
-        (3, 5, 8),
-        (4, 5, 9),
-        (5, 5, 10),
-        (2, 10, 12),
-        (10, 10, 20),
-        (-2, -5, -7),
-
+        (0, 100.0),
+        (50, 50.0),
+        (100, 0.0),
+        (25.5, 74.5),
+        (10, 90.0),
 ]
 @pytest.mark.parametrize("discount_percent,expected",test_data)
 
